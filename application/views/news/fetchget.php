0e548c16dc0a5f3eff7501df2ae9416180b66193
Uzziel Palolan <uepalolan23@student.apc.edu.ph>
2021-07-01 18:08:42 +0800

    added fetchget

---
+++ application/views/news/fetchget.php
@@ -0,0 +1,15 @@
+!DOCTYPE
+<html>
+     <head>
+     </head>
+	 <body>
+	 <script>
+	 const url = 'https://codeigniter422.herokuapp.com/index.php/newsapi/';
+	 fetch(url)
+	 .then(data => data.json())
+	 .then((json) => {
+		 alert(JSON.stringify(json);
+	 })
+	 </script>
+	 </body>
+</html>	 
