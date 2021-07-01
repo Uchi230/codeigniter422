c5de3b66735b548b710f9c94f7729b2a45c1afbb
Uzziel Palolan <uepalolan23@student.apc.edu.ph>
2021-07-01 15:44:31 +0800

    updated Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-use chriskacerguis/codeigniter-restserver;
+use chriskacerguis\RestServer\RestController;
 
 class Api extends RestController {
 
